4a3ffa2028df891cca78d93b402c0ae90f3050ee
zKolait <nicolas.chapeau@devinci.fr>
2019-05-10 14:15:26 +0200

    init

---
+++ src/router.js
@@ -0,0 +1,84 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// Pages
+import Admin from './views/admin'
+import Login from './views/login'
+import Register from './views/register'
+import Home from './views'
+
+// Middlewares
+import auth from './middleware/auth'
+
+// Layouts : { admin }
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      meta: { layout: 'admin', middleware: auth },
+      component: Admin
+    }
+  ]
+})
+
+
+// Middleware code
+//
+// ->
+function nextFactory(context, middleware, index) {
+  const subsequentMiddleware = middleware[index]
+  // If no subsequent Middleware exists,
+  // the default `next()` callback is returned.
+  if (!subsequentMiddleware) return context.next
+
+  return (...parameters) => {
+    // Run the default Vue Router `next()` callback first.
+    context.next(...parameters)
+    // Then run the subsequent Middleware with a new
+    // `nextMiddleware()` callback.
+    const nextMiddleware = nextFactory(context, middleware, index + 1)
+    subsequentMiddleware({ ...context, next: nextMiddleware })
+  }
+}
+
+// Before pushing to page if to has a middleware execute it
+router.beforeEach((to, from, next) => {
+  if (to.meta.middleware) {
+    const middleware = Array.isArray(to.meta.middleware)
+      ? to.meta.middleware
+      : [to.meta.middleware]
+
+    const context = {
+      from,
+      next,
+      router,
+      to,
+    }
+    const nextMiddleware = nextFactory(context, middleware, 1)
+
+    return middleware[0]({ ...context, next: nextMiddleware })
+  }
+
+  next()
+})
+
+export default router
